0e745a78b3b70df08e46ed81ca0cd0451c555751
alaagamal98 <alaagamal.98@gmail.com>
2021-01-15 17:26:47 +0200

    ui connected to server

--- client.py
+++ client.py
@@ -1,86 +1,146 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+from mainwindow import Ui_MainWindow
+import sys
 import socket
 import threading
 import errno
-import sys
-
-# * DEFINING STANDARDS OF THE TCP STREAM CLIENT SOCKET CONNECTION
-HEADER = 64 # * Define a constant size of the header
-PORT = 5050 # * Define a port for the socket that client access
-FORMAT = 'utf-8' # * A decoding/encoding format of the messages
-DISCONNECT_MESSAGE = '!DISCONNECT' # * A defined disconnect message
-SERVER = socket.gethostbyname(socket.gethostname()) # * Define the IPv4 address the client will connect to. 
-ADDRESS = (SERVER, PORT) # * The address to which the client will connect
-idle_status = False
 
 
- 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # * defining a socket object for the client
-client.connect(ADDRESS) # * connecting the client socket to the server socket
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(ApplicationWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-def recieve_status_message():
-    connected = True
-    while connected:
-        status_length = int(client.recv(HEADER).decode(FORMAT)) # * recieve the length of the status message sent from the server
-        status = client.recv(status_length).decode(FORMAT) # * recieve the status message itself from the server
-        print(status)
-        if status == DISCONNECT_MESSAGE:
-            global idle_status
-            idle_status = True
-            connected = False
-            print("[IDLE] Connection was closed please open the UI again")
+        self.questions = ['Do you have a cough?', 'Do you have a fever?', 'Do you have shortness of breath?',
+                          'Are you experiencing any fatigue?', 'Do you have a sore throat?', 'Do you have a runny nose?',
+                          'Do you have any muscle pain?', 'Do you have a headache?', 'Are you experiencing loss of taste or smell?']
+
+        self.questionIndex = 0
+        self.ui.Question.setText(self.questions[self.questionIndex])
+        self.answers = []
+        self.ui.Yes.clicked.connect(lambda: self.answer_question("Yes"))
+        self.ui.No.clicked.connect(lambda: self.answer_question("No"))
+        # * DEFINING STANDARDS OF THE TCP STREAM CLIENT SOCKET CONNECTION
+        self.HEADER = 64  # * Define a constant size of the header
+        self.PORT = 5050  # * Define a port for the socket that client access
+        self.FORMAT = 'utf-8'  # * A decoding/encoding format of the messages
+        self.DISCONNECT_MESSAGE = '!DISCONNECT'  # * A defined disconnect message
+        # * Define the IPv4 address the client will connect to.
+        self.SERVER = socket.gethostbyname(socket.gethostname())
+        # * The address to which the client will connect
+        self.ADDRESS = (self.SERVER, self.PORT)
+        self.idle_status = False
+        # * defining a socket object for the client
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # * connecting the client socket to the server socket
+        self.client.connect(self.ADDRESS)
+        self.timer = QtCore.QTimer(self)
+
+        # adding action to timer
+        self.timer.timeout.connect(self.check_timeout)
+
+        # update the timer every tenth second
+        self.timer.start(100)
+
+        # * execute client handling in a new thread
+        self.thread = threading.Thread(target=self.recieve_status_message)
+        self.thread.start()
+
+    def recieve_status_message(self):
+        connected = True
+        while connected:
+            # * recieve the length of the status message sent from the server
+            status_length = int(self.client.recv(
+                self.HEADER).decode(self.FORMAT))
+            # * recieve the status message itself from the server
+            status = self.client.recv(status_length).decode(self.FORMAT)
+            print(status)
+            if status == self.DISCONNECT_MESSAGE:
+                self.idle_status = True
+                connected = False
+
+    def answer_question(self, answer):
+        if not self.idle_status:
+            if self.questionIndex < 9:
+                message = answer.encode(self.FORMAT)  # * encode the message
+                msg_length = f"{len(message):<{self.HEADER}}".encode(
+                    self.FORMAT)  # * create the header of the message
+                self.client.send(msg_length)  # * send the header first
+                self.client.send(message)  # * send the message
+                self.answers.append(answer)
+                item = QtWidgets.QTableWidgetItem(
+                    "Auto Doctor:   " + str(self.questions[self.questionIndex]))
+                item.setFlags(QtCore.Qt.ItemIsEnabled)
+                self.ui.Chat.setItem(self.questionIndex*2, 0, item)
+                item = QtWidgets.QTableWidgetItem(
+                    str(self.answers[self.questionIndex]) + "   :You")
+                item.setTextAlignment(QtCore.Qt.AlignRight)
+                item.setFlags(QtCore.Qt.ItemIsEnabled)
+                self.ui.Chat.setItem(self.questionIndex*2+1, 1, item)
+                self.questionIndex += 1
+                if self.questionIndex < 9:
+                    self.ui.Question.setText(
+                        self.questions[self.questionIndex])
+
+            if self.questionIndex == 9:
+                if self.ui.Yes.text() == "Okay, thank you Dr.":
+                    if answer == "Yes":
+                        item = QtWidgets.QTableWidgetItem(
+                            self.ui.Yes.text() + "   :You")
+                        item.setTextAlignment(QtCore.Qt.AlignRight)
+                        item.setFlags(QtCore.Qt.ItemIsEnabled)
+                        self.ui.Chat.setItem(self.questionIndex*2+1, 1, item)
+                        self.ui.Yes.setText("")
+                        self.ui.No.setText("")
+                        self.ui.Question.setText("")
+                        self.questionIndex += 1
+                    else:
+                        for i in range(self.questionIndex+1):
+                            item = QtWidgets.QTableWidgetItem("")
+                            item.setFlags(QtCore.Qt.ItemIsEnabled)
+                            self.ui.Chat.setItem(i*2, 0, item)
+                            item = QtWidgets.QTableWidgetItem("")
+                            item.setTextAlignment(QtCore.Qt.AlignRight)
+                            item.setFlags(QtCore.Qt.ItemIsEnabled)
+                            self.ui.Chat.setItem(
+                                i*2+1, 1, item)
+
+                        self.questionIndex = 0
+                        self.ui.Yes.setText("Yes")
+                        self.ui.No.setText("No")
+                        self.ui.Question.setText(
+                            self.questions[self.questionIndex])
+                        self.answers = []
+
+                else:
+                    diagoses = self.diagoses_detection(self.answers)
+                    item = QtWidgets.QTableWidgetItem(
+                        "Auto Doctor:   " + diagoses)
+                    self.ui.Question.setText(diagoses)
+                    item.setFlags(QtCore.Qt.ItemIsEnabled)
+                    self.ui.Chat.setItem(self.questionIndex*2, 0, item)
+                    self.ui.Yes.setText("Okay, thank you Dr.")
+                    self.ui.No.setText("I want to answer the questions again.")
+
+    def diagoses_detection(self, answers_list):
+        print(self.answers)
+        diagoses = "Your predicted diagoses is ....."
+        return diagoses
+
+    def check_timeout(self):
+        if self.idle_status:
+            QtWidgets.QMessageBox.about(
+                self, "Connection Lost", "[IDLE] Connection was closed please open the UI again")
             sys.exit()
 
-        
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    application = ApplicationWindow()
+    application.show()
+    app.exec_()
 
 
-
-def send(msg):
-    """send: a function that sends a message "msg" to the server.
-
-    Args:
-        msg (str): The message to be sent
-    """
-    if not idle_status:
-        message = msg.encode(FORMAT) # * encode the message
-        msg_length = f"{len(message):<{HEADER}}".encode(FORMAT) # * create the header of the message
-        client.send(msg_length) # * send the header first
-        client.send(message) # * send the message
-    else:
-        print("[IDLE] Connection was closed please open the UI again")
-        
-        
-    # client.settimeout(5.0)
-    # status_lenght = ""
-    # try:
-    #     status_lenght = client.recv(HEADER).decode(FORMAT)
-    # except socket.timeout as e:
-    #     print("[TIME OUT] Timed out after 5 seconds")
-    #     client.send(DISCONNECT_MESSAGE)
-    # print(status_lenght)
-    # if status_lenght:
-    #     status_lenght = int(status_lenght)
-    #     status = client.recv(status_lenght).decode(FORMAT)
-    #     print(status)
-    # else:
-    #     print("Didn't recieve a message")
-    
-thread = threading.Thread(target=recieve_status_message) # * execute client handling in a new thread
-thread.start()
-send("YES")
-input()
-send("YES")
-input()
-send("What")
-input()
-send("YES")
-input()
-send("YES")
-input()
-send("NO")
-input()
-send("YES")
-input()
-send("YES")
-input()
-send("NO")
-send(DISCONNECT_MESSAGE)
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,20 +1,23 @@
-import socket 
+import socket
 import threading
 
 
 # * DEFINING STANDARDS OF THE TCP STREAM SOCKET CONNECTION
-HEADER = 64 # * Define a constant size of the header
-PORT = 5050 # * Define a port for the socket
-SERVER = socket.gethostbyname(socket.gethostname()) # * Define the IPv4 address the socket will bind to. Here we use the local host
-ADDRESS = (SERVER, PORT) # * Address tuple to use for the binding 
-FORMAT = 'utf-8' # * A decoding/encoding format of the messages
-DISCONNECT_MESSAGE = '!DISCONNECT' # * A defined disconnect message
+HEADER = 64  # * Define a constant size of the header
+PORT = 5050  # * Define a port for the socket
+# * Define the IPv4 address the socket will bind to. Here we use the local host
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDRESS = (SERVER, PORT)  # * Address tuple to use for the binding
+FORMAT = 'utf-8'  # * A decoding/encoding format of the messages
+DISCONNECT_MESSAGE = '!DISCONNECT'  # * A defined disconnect message
 IDLE_MESSAGE = 'IDLE'
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # * Defining a socket object with address family of IPv4 and socket type for TCP using SOCK_STREAM
-server.bind(ADDRESS) # * Associate the socket to a specific network and port
+# * Defining a socket object with address family of IPv4 and socket type for TCP using SOCK_STREAM
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDRESS)  # * Associate the socket to a specific network and port
 
-clients_database = {} # * A dictionary holds the answers of each client
+clients_database = {}  # * A dictionary holds the answers of each client
+
 
 def handle_client(conn, addr):
     """handle_client: a function that is executed in a separate thread for each client. it assigns a list for each unique client that contains the answers of the clients,
@@ -24,37 +27,45 @@ def handle_client(conn, addr):
         conn (socket.socket): the client socket
         addr (tuple): the address of the client
     """
-    print(f"[NEW CONNECTION] {addr} connected.") 
-    connected = True # ! setting the connection status to True
-    clients_database[addr[1]] = [] # ! assigning a new list in the clients dictionary
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True  # ! setting the connection status to True
+    # ! assigning a new list in the clients dictionary
+    clients_database[addr[1]] = []
     while connected:
         try:
             conn.settimeout(10.0)
-            msg_length = conn.recv(HEADER).decode(FORMAT) # * Recieve the header of the message
+            # * Recieve the header of the message
+            msg_length = conn.recv(HEADER).decode(FORMAT)
             conn.settimeout(None)
-            if msg_length: # ? if the message isn't empty
-                msg_lenght = int(msg_length) # * Get the actual length of the message
+            if msg_length:  # ? if the message isn't empty
+                # * Get the actual length of the message
+                msg_lenght = int(msg_length)
                 msg = conn.recv(msg_lenght).decode(FORMAT)
                 if msg == DISCONNECT_MESSAGE:
                     print(f"[DISCONNECTED] {addr} has disconnected")
-                    connected = False # ! setting the connection status to False, exit the loop and close the connection
+                    # ! setting the connection status to False, exit the loop and close the connection
+                    connected = False
                     continue
                 print(f"[{addr}] sent a message : {msg}")
-                if msg == 'YES':
-                    clients_database[addr[1]].append(1) # ? If the message is "YES", append a 1 to the list of the answers
-                    conn.send(create_status_message(addr[1], "VALID")) # * Send a status message to the client confirming recieving a valid response
-                elif msg == 'NO':
-                    clients_database[addr[1]].append(0) # ? If the message is "NO", append a 0 to the list of the answers
-                    conn.send(create_status_message(addr[1], "VALID")) # * Send a status message to the client confirming recieving a valid response
+                if msg == 'Yes':
+                    # ? If the message is "YES", append a 1 to the list of the answers
+                    clients_database[addr[1]].append(1)
+                    # * Send a status message to the client confirming recieving a valid response
+                    conn.send(create_status_message(addr[1], "VALID"))
+                elif msg == 'No':
+                    # ? If the message is "NO", append a 0 to the list of the answers
+                    clients_database[addr[1]].append(0)
+                    # * Send a status message to the client confirming recieving a valid response
+                    conn.send(create_status_message(addr[1], "VALID"))
                 else:
-                    conn.send(create_status_message(addr[1], "INVALID")) # * Send a status message to the client confirming recieving an invalid response
+                    # * Send a status message to the client confirming recieving an invalid response
+                    conn.send(create_status_message(addr[1], "INVALID"))
         except socket.timeout as e:
             print("[TIME OUT] Timed out after 10 seconds")
             conn.send(create_status_message(addr[1], "IDLE"))
             connected = False
-            
 
-    conn.close() # ! Closing the connection when exiting the stream or recieving an IDLE
+    conn.close()  # ! Closing the connection when exiting the stream or recieving an IDLE
 
 
 def create_status_message(label, condition):
@@ -68,9 +79,11 @@ def create_status_message(label, condition):
         str: encoded status message
     """
     if condition == "VALID":
-        status_message = f'[CONFIRMED] {10 - len(clients_database[label])} questions left' # ? If valid, create a confirmation message with the number of questions left
+        # ? If valid, create a confirmation message with the number of questions left
+        status_message = f'[CONFIRMED] {9 - len(clients_database[label])} questions left'
     elif condition == "INVALID":
-        status_message = f'[UNVALID] Please send an appropiate response' # ? If not valid, create a confirmation of invalidaty message
+        # ? If not valid, create a confirmation of invalidaty message
+        status_message = f'[UNVALID] Please send an appropiate response'
     elif condition == "IDLE":
         status_message = DISCONNECT_MESSAGE
 
@@ -80,18 +93,20 @@ def create_status_message(label, condition):
     status_encoded = status.encode(FORMAT)
     return status_encoded
 
+
 def start():
     """starting the server
     """
     print("[STARTING] server is starting ...")
-    server.listen() # * start the server on the network to which it was binded 
+    server.listen()  # * start the server on the network to which it was binded
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        conn, addr = server.accept() # * accept clients and take their sockets and addresses
-        thread = threading.Thread(target=handle_client, args=(conn, addr)) # * execute client handling in a new thread
+        conn, addr = server.accept()  # * accept clients and take their sockets and addresses
+        # * execute client handling in a new thread
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1} clients have connected ...")
+        print(
+            f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1} clients have connected ...")
 
 
 start()
-
